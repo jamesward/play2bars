10d2d041e6505b391358ca07dc384a6de154b9b0
Ryan Knight <ryan@grandcloud.com>
2012-01-24 16:51:02 -0700

    fixing json code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,8 +2,8 @@ package controllers;
 
 
 import models.Bar;
+import org.codehaus.jackson.JsonNode;
 import play.data.Form;
-import play.mvc.Content;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -12,25 +12,25 @@ import static play.libs.Json.toJson;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Enter the name of your new bar:", form(Bar.class)));
-    }
+   public static Result index() {
+      return ok(index.render("2 Enter the name of your new bar:", form(Bar.class)));
+   }
 
-    public static Result addBar() {
-        Form<Bar> form = form(Bar.class).bindFromRequest();
-        if (form.hasErrors()) {
-            return badRequest(index.render("Error in Bar form.  Enter new Bar", form(Bar.class)));
-        } else {
-            Bar bar = form.get();
-            bar.save();
-            return ok(
-                    index.render("The bar  " + bar.name + " was saved.   Enter a new Bar:", form(Bar.class))
-            );
-        }
-    }
+   public static Result addBar() {
+      Form<Bar> form = form(Bar.class).bindFromRequest();
+      if (form.hasErrors()) {
+         return badRequest(index.render("Error in Bar form.  Enter new Bar", form(Bar.class)));
+      } else {
+         Bar bar = form.get();
+         bar.save();
+         return ok(
+                 index.render("2 The bar  " + bar.name + " was saved.   Enter a new Bar:", form(Bar.class))
+         );
+      }
+   }
 
-    public static Result listBars() {
-       Content content = toJson(Bar.findAll());
-       return ok(content);
-    }
+   public static Result listBars() {
+      JsonNode jsonNodes = toJson(Bar.findAll());
+      return ok(jsonNodes);
+   }
 }
