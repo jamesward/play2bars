70708ab3b871ced3beab3fbe3d4d5fb168da34a3
James Ward <james@jamesward.org>
2012-09-11 07:23:36 -0600

    upgrade play

--- project/Build.scala
+++ project/Build.scala
@@ -4,17 +4,16 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "play2bars"
+    val appName         = "play2bars-mongodb"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
       "com.jquery" % "jquery" % "1.7.1",
-      "net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0-rc.4"
+      "net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       resolvers += "webjars" at "http://webjars.github.com/m2"
-      // Add your own project settings here      
     )
 
 }
