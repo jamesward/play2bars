bd84379a59dff9fd7ad78d7f60d58b2ff0aa4ac1
James Ward <james@jamesward.org>
2012-10-22 11:20:00 -0600

    mysql example

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: target/start -Dhttp.port=${PORT} -Dconfig.file=conf/postgresql.conf ${JAVA_OPTS}
+web: target/start -Dhttp.port=${PORT} -Dconfig.file=conf/mysql.conf ${JAVA_OPTS}
--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "postgresql" % "postgresql" % "9.0-801.jdbc3"
+      "mysql" % "mysql-connector-java" % "5.1.21"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
